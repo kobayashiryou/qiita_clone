9e4cd0b4f6b20082d1f687137897c178499dc624
kobayashiryou <chiri.parihead@gmail.com>
2021-06-14 17:20:55 +0900

    各モデルのテストの修正

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,21 +1,21 @@
 require "rails_helper"
 
 RSpec.describe Comment, type: :model do
-  context "contentが指定されている時とされていない時" do
-    before do
-      @user = create(:user)
-      @article = create(:article, user_id: @user.id)
-    end
+  context "validation check" do
+    context "contentが指定されている時とされていない時" do
+      let!(:user){ create(:user) }
+      let!(:article){ create(:article, user_id: user.id) }
 
-    it "contentが指定されているとcommentが作成される" do
-      comment = create(:comment, user_id: @user.id, article_id: @article.id)
-      expect(comment).to be_valid
-    end
+      it "contentが指定されているとcommentが作成される" do
+        comment = create(:comment, user_id: user.id, article_id: article.id)
+        expect(comment).to be_valid
+      end
 
-    it "contentが指定されていない時にエラーする" do
-      comment = build(:comment, user_id: @user.id, article_id: @article.id, content: nil)
-      comment.valid?
-      expect(comment.errors.messages[:content]).to include "can't be blank"
+      it "contentが指定されていない時にエラーする" do
+        comment = build(:comment, user_id: user.id, article_id: article.id, content: nil)
+        comment.valid?
+        expect(comment.errors.messages[:content]).to include "can't be blank"
+      end
     end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,59 +1,53 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
-  context "nickname,email,passwordを指定している時" do
-    it "ユーザーが作られる" do
-      user = create(:user)
-      expect(user).to be_valid
+  context "validation check" do
+    context "nicknameを指定している時" do
+      it "ユーザーが作られる" do
+        user = create(:user)
+        expect(user).to be_valid
+      end
     end
-  end
 
-  context "nicknameを指定していない時" do
-    it "エラーする" do
-      user = build(:user, nickname: nil)
-      user.valid?
-      expect(user.errors.messages[:nickname]).to include "can't be blank"
+    context "nicknameを指定していない時" do
+      it "エラーする" do
+        user = build(:user, nickname: nil)
+        user.valid?
+        expect(user.errors.messages[:nickname]).to include "can't be blank"
+      end
     end
-  end
 
-  context "同じnicknameが存在している時" do
-    it "エラーする" do
-      create(:user, nickname: "harry")
-      user = build(:user, nickname: "harry")
-      user.valid?
-      expect(user.errors.messages[:nickname]).to include "has already been taken"
+    context "同じnicknameが存在している時" do
+      it "エラーする" do
+        create(:user, nickname: "harry")
+        user = build(:user, nickname: "harry")
+        user.valid?
+        expect(user.errors.messages[:nickname]).to include "has already been taken"
+      end
     end
-  end
 
-  context "emailを指定していない時" do
-    it "エラーする" do
-      user = build(:user, email: nil)
-      user.valid?
-      expect(user.errors.messages[:email]).to include "can't be blank"
+    context "emailを指定していない時" do
+      it "エラーする" do
+        user = build(:user, email: nil)
+        user.valid?
+        expect(user.errors.messages[:email]).to include "can't be blank"
+      end
     end
-  end
 
-  context "passwordを指定していない時" do
-    it "エラーする" do
-      user = build(:user, password: nil)
-      user.valid?
-      expect(user.errors.messages[:password]).to include "can't be blank"
+    context "passwordを指定していない時" do
+      it "エラーする" do
+        user = build(:user, password: nil)
+        user.valid?
+        expect(user.errors.messages[:password]).to include "can't be blank"
+      end
     end
-  end
 
-  context "passwordが３２文字を超過している時" do
-    it "エラーする" do
-      user = build(:user, password: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
-      user.valid?
-      expect(user.errors.messages[:password]).to include "is too long (maximum is 32 characters)"
-    end
-  end
-
-  context "passwordが８文字未満の時" do
-    it "エラーする" do
-      user = build(:user, password: "aaaaaaa")
-      user.valid?
-      expect(user.errors.messages[:password]).to include "is too short (minimum is 8 characters)"
+    context "passwordが6文字未満の時" do
+      it "エラーする" do
+        user = build(:user, password: "aaa")
+        user.valid?
+        expect(user.errors.messages[:password]).to include "is too short (minimum is 6 characters)"
+      end
     end
   end
 end
