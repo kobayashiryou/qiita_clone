f21a9644e2886e951361615846c9d9ccf3926392
kobayashiryou <chiri.parihead@gmail.com>
2021-06-26 20:52:36 +0900

    articlesのURL指定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   get "users/index"
-  resources :articles
+  scope :api do
+    scope :v1 do
+      resources :articles
+    end
+  end
   mount_devise_token_auth_for "User", at: "auth"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
