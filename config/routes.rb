e29fb94e082cb6515da4b372f6fcb83ae6e4680e
kobayashiryou <chiri.parihead@gmail.com>
2021-06-27 11:17:20 +0900

    namespaceを利用したURLの指定

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get "users/index"
-  scope :api do
-    scope :v1 do
+  namespace :api, format: 'json' do
+    namespace :v1 do
       resources :articles
+      mount_devise_token_auth_for "User", at: "auth"
     end
   end
-  mount_devise_token_auth_for "User", at: "auth"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
