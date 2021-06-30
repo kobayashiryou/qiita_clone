a14bea09cd688add51a3f6fcbbf292582cf5164c
kobayashiryou <chiri.parihead@gmail.com>
2021-06-30 16:28:04 +0900

    current_userの仮実装

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,3 +1,8 @@
 class Api::V1::ApiController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
+  protect_from_forgery with: :null_session
+
+  def current_user
+    @current_user ||= User.first
+  end
 end
