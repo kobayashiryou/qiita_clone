42b71b8453bb75fd6b07361c8f333d2255c5a6b1
kobayashiryou <chiri.parihead@gmail.com>
2021-06-30 16:33:12 +0900

    記事作成のrequest spec

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -36,4 +36,21 @@ RSpec.describe "Articles", type: :request do
       end
     end
   end
+
+  describe "POST api/v1/articles" do
+    subject{ post(api_v1_articles_path, params: params) }
+    let(:current_user){ create(:user) }
+    let(:params){ {article: attributes_for(:article) } }
+
+    before do
+      allow_any_instance_of(Api::V1::ApiController).to receive(:current_user).and_return(current_user)
+    end
+
+      context "current_userが記事を作成した時" do
+      it "記事は作成される" do
+        expect{ subject }.to change { current_user.articles.count }.by(1)
+        expect(response).to have_http_status(200)
+      end
+    end
+  end
 end
