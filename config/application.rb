ebecaf918f9cfead0f5b190730e0d31e0a3a0e01
kobayashiryou <chiri.parihead@gmail.com>
2021-07-10 08:41:08 +0900

    bundle updateによるconfig/applicationの編集

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,8 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+require "active_model_serializers"
+
 module QiitaClone
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
