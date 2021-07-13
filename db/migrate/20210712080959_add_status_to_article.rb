144bf7556b81fb87bd626edb3d1144e60bda3ae7
kobayashiryou <chiri.parihead@gmail.com>
2021-07-13 12:01:17 +0900

    rails db:rollback & change migration file

--- db/migrate/20210712080959_add_status_to_article.rb
+++ db/migrate/20210712080959_add_status_to_article.rb
@@ -1,5 +1,5 @@
 class AddStatusToArticle < ActiveRecord::Migration[6.0]
   def change
-    add_column :articles, :status, :boolean, default: true, null: false
+    add_column :articles, :status, :string, default: "published", null: false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2021_07_12_080959) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
-    t.boolean "status", default: true, null: false
+    t.string "status", default: "published", null: false
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
