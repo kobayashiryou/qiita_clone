5d08bd2f319fdf51e8567ab0e0883321d8fd0cb5
kobayashiryou <chiri.parihead@gmail.com>
2021-07-14 09:10:09 +0900

    add validates in article_model

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,6 @@ class Article < ApplicationRecord
   belongs_to :user
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
-  validates :title, :body, presence: true
+  validates :title, :body, :status, presence: true
   enum status: { draft: "draft", published: "published" }
 end
