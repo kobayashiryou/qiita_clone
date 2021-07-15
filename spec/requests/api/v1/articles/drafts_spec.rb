8d3a7aa4e1bc793ab5a3e7f056fdc5f30bd85e6c
kobayashiryou <chiri.parihead@gmail.com>
2021-07-15 13:32:05 +0900

    fix drafts_spec

--- spec/requests/api/v1/articles/drafts_spec.rb
+++ spec/requests/api/v1/articles/drafts_spec.rb
@@ -2,10 +2,11 @@ require "rails_helper"
 
 RSpec.describe "Drafts", type: :request do
   describe "GET api/v1/articles/draft" do
-    subject{ get(api_v1_articles_drafts_path) }
-
+    subject{ get(api_v1_articles_drafts_path, headers: headers) }
+    let!(:current_user){ create(:user) }
+    let(:headers){ current_user.create_new_auth_token }
     before do
-      create_list(:article, 3, status: :draft)
+      create_list(:article, 3, status: :draft, user: current_user)
     end
     context "statusがdraftの時" do
       it "下書き記事一覧が表示される" do
@@ -20,8 +21,10 @@ RSpec.describe "Drafts", type: :request do
   end
 
   describe "GET api/v1/articles/draft/:id" do
-    subject{ get(api_v1_articles_draft_path(article_id) ) }
-    let!(:article){ create(:article, status: :draft) }
+    subject{ get(api_v1_articles_draft_path(article_id), headers: headers ) }
+    let!(:current_user){ create(:user) }
+    let(:headers){ current_user.create_new_auth_token }
+    let(:article){ create(:article, status: :draft, user: current_user) }
     let(:article_id){ article.id }
     context "指定したidのstatusがdraftだった時" do
       it "指定したidの下書き記事が表示される" do
