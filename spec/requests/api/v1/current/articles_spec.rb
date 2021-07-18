2c602a72236a16551676999255a098489e9f6146
kobayashiryou <chiri.parihead@gmail.com>
2021-07-18 16:07:28 +0900

    execution rubocop -a

--- spec/requests/api/v1/current/articles_spec.rb
+++ spec/requests/api/v1/current/articles_spec.rb
@@ -2,11 +2,11 @@ require "rails_helper"
 
 RSpec.describe "Mypage", type: :request do
   describe "GET api/v1/current/articles" do
-    subject{ get(api_v1_current_articles_path, headers: headers) }
+    subject { get(api_v1_current_articles_path, headers: headers) }
 
-    let!(:current_user){ create(:user) }
-    let!(:user){ create(:user) }
-    let(:headers){ current_user.create_new_auth_token }
+    let!(:current_user) { create(:user) }
+    let!(:user) { create(:user) }
+    let(:headers) { current_user.create_new_auth_token }
     before do
       create_list(:article, 3, user: current_user)
       create_list(:article, 2, user: current_user, status: :draft)
