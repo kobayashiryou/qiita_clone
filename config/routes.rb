2353a5571c8cf0e063bf3c7cc6c8ce846cdb9c3f
kobayashiryou <chiri.parihead@gmail.com>
2021-07-19 09:20:37 +0900

    MyPageのルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get "articles/new", to: "homes#index"
   get "articles/:id/edit", to: "homes#index"
   get "articles/:id", to: "homes#index"
-  get "articles/mypage", to: "homes#index"
+  get "mypage", to: "homes#index"
 
 
   namespace :api, format: "json" do
